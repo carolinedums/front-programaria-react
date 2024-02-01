02b2ebd9cd8bdc40ef7fcb2fff80515619aaf327
carolinedums <caroline.dums.lima@gmail.com>
2024-02-01 17:59:05 -0300

    reverting test url

--- front-programaria-react-main/src/components/Content.jsx
+++ front-programaria-react-main/src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'http://localhost:3333/mulheres'
+  const baseURL = 'http://beck-end-7ys1.onrender.com/mulheres'
 
   useEffect(() => {
     async function getData() {
